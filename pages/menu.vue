da4c8bb4891130f89c76523566936f8bf22259eb
fet1sov <august9222@gmail.com>
2025-03-27 02:59:20 +0300

    feat(mobile-menu): markdown the vertical menu (#18)
    markdown for the mobile vertical menu
    
    Refs: #18

--- pages/menu.vue
+++ pages/menu.vue
@@ -1,12 +1,13 @@
 <script lang="ts" setup>
-
+import { UserMenuItems } from '~/content/usermenu/UserMenuItems';
 </script>
 
 <template>
     <MobileOnly>
-        <div class="p-[20px]">
+        <div 
+            class="p-[20px] bg-secondary-secondary"
+            v-if="!useCheckUserAuth()">
             <RouterLink 
-                v-if="!useCookie('access_token').value"
                 to="/auth">
                 <p class="w-full py-[10px] text-center font-bold text-[12px] bg-primary-primary hover:bg-primary-secondary transition-all duration-300 rounded-[5px] text-secondary-primary">
                     {{ $t("auth.buttons.login") }}
@@ -14,8 +15,21 @@
             </RouterLink>
         </div>
 
-        <ul>
-            <li></li>
+        <div 
+            class="bg-secondary-secondary" 
+            v-else>
+            
+        </div>
+
+        <ul v-if="useCheckUserAuth()">
+            <li class="flex flex-row mx-[10px] border-secondary-wrapper-light border-b-[1px] py-[15px] items-center hover:[&>*]:fill-primary-primary" v-for="item in UserMenuItems">
+                <NuxtLink 
+                :class="'flex flex-row items-center transition-all transition-250 hover:[&>*]:fill-primary-primary hover:[&>*]:text-primary-primary ' + ''" 
+                :to="item.route">
+                    <component class="mr-[10px] w-[22px] h-[22px]" :is="item.icon"></component>
+                    <p :class="'font-bold transition-all transition-250 ' + (item.color ? 'text-' + item.color + '' : '')">{{ item.label }}</p>
+                </NuxtLink>
+            </li>
         </ul>
     </MobileOnly>
 </template>
