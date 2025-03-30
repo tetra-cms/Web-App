5869c97296b141eaca5258a6fc699ed72855b80f
fet1sov <august9222@gmail.com>
2025-03-31 00:28:13 +0300

    feat(cart): mobile markdown for a cart page (#15)
    Refs: #15

---
+++ components/orderaction/OrderAction.vue
@@ -0,0 +1,20 @@
+<script lang="ts" setup>
+const props = defineProps<{
+    amount: number,
+    summary: number
+}>();
+</script>
+
+<template>
+    <div class="w-full flex flex-row justify-center fixed bottom-[60px] pr-[25px] bg-secondary-secondary">
+        <button 
+            class="flex text-[10pt] flex-row items-center text-center justify-between select-none text-secondary-primary font-semibold p-[5px] rounded-[5px] bg-primary-primary w-full">
+                <p class="font-normal">
+                    {{ props.amount }} 
+                    {{ pluralizeWord($t("labels.item.singular"), $t("labels.item.plural"), props.amount) }}
+                </p>
+                <p>{{ $t("buttons.placeorder") }}</p>
+                <p class="font-normal">{{ props.summary.toLocaleString() }} ₽</p>
+        </button>
+    </div>
+</template>
--- pages/cart.vue
+++ pages/cart.vue
@@ -21,16 +21,50 @@ cartItems.value = !import.meta.client || JSON.parse(localStorage.getItem("cart")
         <Header 
             :title="String(CompanyData.title)"
             :subtitle="String(CompanyData.subtitle)"></Header>
+
+        <div class="w-full flex flex-col justify-between">
+            <div class="flex flex-row justify-between px-[60px] mb-[20px]">
+                <h1 class="font-druk text-[24px] font-bold">{{ $t('headers.cart') }}</h1>
+
+                <div></div>
+            </div>
+        </div>
     </DesktopOnly>
     
     <DesktopOnly>
         <div class="flex flex-row">
-            <ul>
-                <li
+            <table>
+                <thead>
+                    <tr class="bg-secondary-secondary">
+                        <th>{{ $t('tables.cart.name') }}</th>
+                        <th>{{ $t('tables.cart.amount') }}</th>
+                        <th>{{ $t('tables.cart.price') }}</th>
+                        <th>
+                            <button>
+                                {{ $t('tables.selectall') }}
+                            </button>
+                        </th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    <tr
+                    class="border-secondary-secondary border-b-[2px]"
                     v-for="cartItem in cartItems">
-                    <p>{{ cartItem.productInfo.name }}</p>
-                </li>
-            </ul>
+                        <th>
+                            <div class="flex flex-row items-center">
+                                <img class="w-[64px]" :src="cartItem.productInfo.imageUrl">
+                                <p class="font-normal">{{ cartItem.productInfo.name }}</p>
+                            </div>
+                        </th>
+                        <th><CartButton :id="Number(cartItem.productInfo.id)"/></th>
+                        <th>{{ cartItem.productInfo.price.toLocaleString() }} ₽</th>
+                        <th>
+
+                        </th>
+                    </tr>
+                </tbody>
+            </table>
 
             <div 
                 class="w-[200px] py-[40px] bg-secondary-secondary px-[15px] rounded-[10px]">
@@ -58,7 +92,35 @@ cartItems.value = !import.meta.client || JSON.parse(localStorage.getItem("cart")
 
     <MobileOnly>
         <div>
+            <div class="flex flex-start flex-row items-center">
+                <h1 class="text-[24px] font-bold">{{ $t('headers.cart') }}</h1>
+                <p class="text-secondary-wrapper-light ml-[10px]">
+                    {{ cartItems.length }} 
+                    {{ pluralizeWord($t("labels.item.singular"), $t("labels.item.plural"), cartItems.length) }}
+                </p>
+            </div>
+            
 
+            <ul>
+                <li
+                class="flex flex-col mx-[5px] my-[10px] p-[10px] rounded-[10px] flex-row bg-secondary-secondary"
+                v-for="cartItem in cartItems">
+                    <div class="flex flex-row">
+                        <div>
+                            <img class="w-[96px]" :src="cartItem.productInfo.imageUrl">
+                        </div>
+                        
+                        <div class="flex flex-col">
+                            <p class="text-[14pt]">{{ cartItem.productInfo.name }}</p>
+
+                            <p>{{ cartItem.productInfo.price.toLocaleString() }} ₽</p>
+                        </div>
+                    </div>
+                    
+
+                    <OrderAction :amount="cartItems.length" :summary="cart.summary"/>
+                </li>
+            </ul>
         </div>
     </MobileOnly>
 </template>
