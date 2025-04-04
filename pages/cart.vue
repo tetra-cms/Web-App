70db6e7e5b94576e9cc8a28877459be023078c30
fet1sov <august9222@gmail.com>
2025-04-04 19:11:40 +0300

    feat(cart): add items amount handler for desktop view (#15)
    Refs: #15

--- pages/cart.vue
+++ pages/cart.vue
@@ -20,85 +20,98 @@ cartItems.value = !import.meta.client || JSON.parse(localStorage.getItem("cart")
 
         <Header :title="String(CompanyData.title)" :subtitle="String(CompanyData.subtitle)"></Header>
 
-        <div class="w-full flex flex-col justify-between">
-            <div class="flex flex-row justify-between px-[60px] mb-[20px]">
-                <h1 class="font-druk text-[24px] font-bold">{{ $t('headers.cart') }}</h1>
+        <div v-if="cartItems.length">
+            <div class="w-full flex flex-col justify-between">
+                <div class="flex flex-row justify-between px-[60px] mb-[20px]">
+                    <h1 class="font-druk text-[24px] font-bold">{{ $t('headers.cart') }}</h1>
 
-                <div></div>
-            </div>
-        </div>
-    </DesktopOnly>
-
-    <DesktopOnly>
-        <div class="w-full flex flex-col px-[70px] justify-center">
-            <button class="flex flex-row items-center font-normal text-[12pt] mb-[15px]">
-                <div class="w-[24px] h-[24px] mr-[10px] flex justify-center bg-[red] rounded-[5px]">
-                    <TrashIcon />
+                    <div></div>
                 </div>
+            </div>
 
-                <p class="text-[red]">{{ $t('tables.cart.clear') }}</p>
-            </button>
 
-            <div class="w-full flex flex-row">
-                <table class="w-full text-center">
-                    <thead>
-                        <tr class="rounded-[30px] bg-secondary-secondary">
-                            <th>{{ $t('tables.cart.name') }}</th>
-                            <th>{{ $t('tables.cart.amount') }}</th>
-                            <th>{{ $t('tables.cart.price') }}</th>
-                            <th></th>
-                        </tr>
-                    </thead>
+            <div class="w-full flex flex-col px-[70px] justify-center">
+                <button class="flex flex-row items-center font-normal text-[12pt] mb-[15px]">
+                    <div class="w-[24px] h-[24px] mr-[10px] flex justify-center bg-[red] rounded-[5px]">
+                        <TrashIcon />
+                    </div>
 
-                    <tbody>
-                        <tr class="w-full py-[15px] border-secondary-secondary border-b-[2px]"
-                            v-for="cartItem in cartItems">
-                            <td class="py-[15px]">
-                                <div class="flex flex-row items-center">
-                                    <img class="w-[64px] mr-[5px]" :src="cartItem.productInfo.imageUrl">
-                                    <p class="font-normal">{{ cartItem.productInfo.name }}</p>
+                    <p class="text-[red]">{{ $t('tables.cart.clear') }}</p>
+                </button>
+
+                <div class="w-full flex flex-row">
+                    <table class="w-full text-center">
+                        <thead>
+                            <tr class="rounded-[30px] bg-secondary-secondary">
+                                <th>{{ $t('tables.cart.name') }}</th>
+                                <th>{{ $t('tables.cart.amount') }}</th>
+                                <th>{{ $t('tables.cart.price') }}</th>
+                                <th></th>
+                            </tr>
+                        </thead>
+
+                        <tbody>
+                            <tr class="w-full py-[15px] border-secondary-secondary border-b-[2px]"
+                                v-for="cartItem in cartItems">
+                                <td class="py-[15px]">
+                                    <div class="flex flex-row items-center">
+                                        <img class="w-[64px] mr-[5px]" :src="cartItem.productInfo.imageUrl">
+                                        <p class="font-normal">{{ cartItem.productInfo.name }}</p>
+                                    </div>
+                                </td>
+
+                                <td>
+                                    <CartButton :id="Number(cartItem.productInfo.id)" />
+                                </td>
+
+                                <td class="font-bold">{{ cartItem.productInfo.price.toLocaleString() }} ₽</td>
+
+                                <td>
+                                    <button @click="">
+
+                                    </button>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+
+
+                    <div class="flex flex-col w-[230px] ml-[20px]">
+                        <div class="w-full py-[40px] bg-secondary-secondary px-[15px] rounded-[10px]">
+                            <div class="my-[15px]">
+                                <div class="flex flex-row justify-between">
+                                    <p>{{ $t("order.block.summary") }}:</p>
+                                    <p class="font-bold">{{ cart.summary.toLocaleString() }} ₽</p>
                                 </div>
-                            </td>
 
-                            <td>
-                                <CartButton :id="Number(cartItem.productInfo.id)" />
-                            </td>
-
-                            <td class="font-bold">{{ cartItem.productInfo.price.toLocaleString() }} ₽</td>
-
-                            <td>
-                                <button @click="">
-
-                                </button>
-                            </td>
-                        </tr>
-                    </tbody>
-                </table>
-
-
-                <div class="flex flex-col w-[230px] ml-[20px]">
-                    <div class="w-full py-[40px] bg-secondary-secondary px-[15px] rounded-[10px]">
-                        <div class="my-[15px]">
-                            <div class="flex flex-row justify-between">
-                                <p>{{ $t("order.block.summary") }}:</p>
-                                <p class="font-bold">{{ cart.summary.toLocaleString() }} ₽</p>
+                                <p v-if="cartItems.length" class="text-secondary-wrapper-light">
+                                    {{ cartItems.length }}
+                                    {{ pluralizeWord($t("labels.item.singular"), $t("labels.item.plural"), cartItems.length)
+                                    }}
+                                </p>
                             </div>
 
-                            <p v-if="cartItems.length" class="text-secondary-wrapper-light">
-                                {{ cartItems.length }}
-                                {{ pluralizeWord($t("labels.item.singular"), $t("labels.item.plural"), cartItems.length)
-                                }}
-                            </p>
+                            <Button :label="$t('buttons.placeorder')" attributes="w-full py-[10px]" />
                         </div>
 
-                        <Button :label="$t('buttons.placeorder')" attributes="w-full py-[10px]" />
+                        <div class="mt-[10px] bg-secondary-secondary">
+
+                        </div>
                     </div>
 
-                    <div class="mt-[10px] bg-secondary-secondary">
-
-                    </div>
                 </div>
+            </div>
+        </div>
+        
+        <div v-else>
+            <div class="w-full flex flex-col text-center items-center">
+                <CartIcon class="[&>*]:fill-primary-primary w-[128px] h-[128px]"/>
+                <p class="text-primary-primary uppercase font-bold text-[16pt]">{{ $t("tables.cart.emptycart.title") }}</p>
+                <p class="text-[14pt]">{{ $t("tables.cart.emptycart.subtitle") }}</p>
 
+                <RouterLink
+                class="mt-[20px] flex flex-row items-center text-center justify-center select-none text-secondary-primary font-semibold p-[10px] rounded-[30px] bg-primary-primary"
+                to="/">{{ $t("buttons.backincatalog") }}</RouterLink>
             </div>
         </div>
     </DesktopOnly>
