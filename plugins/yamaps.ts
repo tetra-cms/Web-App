ec2a589cae900da86a0a667f4d0cd969bb612d7a
fet1sov <august9222@gmail.com>
2026-07-09 10:28:44 +0500

    fix(yamaps): исправить проблемы с ключём

--- plugins/yamaps.ts
+++ plugins/yamaps.ts
@@ -1,8 +1,16 @@
 import { defineNuxtPlugin } from '#app';
 import { createYmaps } from 'vue-yandex-maps';
- 
+
 export default defineNuxtPlugin((nuxtApp) => {
-    nuxtApp.vueApp.use(createYmaps({
-        apikey: String(nuxtApp.$config.public.yaApiKey)
-    }))
+    const apiKey = nuxtApp.$config.public.yaApiKey;
+
+    if (!apiKey) {
+        return;
+    }
+
+    nuxtApp.vueApp.use(
+        createYmaps({
+            apikey: apiKey,
+        }),
+    );
 });
