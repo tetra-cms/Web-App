c43b24ac6981d321f5e4e4e5ed8cc67cf016b850
fet1sov <august9222@gmail.com>
2026-07-22 11:26:50 +0500

    chore(upgrade): upgrade nuxt and all dependencies to a new version

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
-  ssr: false,
+  ssr: process.env.BASE_SSR ?? true,
   app: {
     head: {
       charset: 'utf-8',
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,43 +1,56 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-    theme: {
-        extend: {
-            colors: {
-                'primary': {
-                    'primary': '#FF8800',
-                    'secondary': '#FFAF54'
-                },
-                'secondary': {
-                    'primary': '#FFFFFF',
-                    'secondary': '#F9F8F8',
-                    'light': '#EFEFEF',
-                    'wrapper': {
-                        'dark': '#000000',
-                        'light': '#DCDCDC'
-                    }
-                },
-                'tomato': {
-                    'primary': '#DA0404',
-                    'secondary': '#C70000'
-                },
-                'dark': {
-                    'secondary': {
-                        'primary': '#000000',
-                        'secondary': '',
-                        'light': '',
-                        'wrapper': {
-                            'dark': '#FFFFFF',
-                            'light': ''
-                        }
-                    }
-                }
-            },
-            
+  content: [
+    './components/**/*.{vue,js,ts}',
+    './layouts/**/*.vue',
+    './pages/**/*.vue',
+    './plugins/**/*.{js,ts}',
+    './composables/**/*.{js,ts}',
+    './app.vue',
+    './error.vue'
+  ],
+
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          primary: '#FF8800',
+          secondary: '#FFAF54'
         },
-        fontFamily: {
-            'druk': 'DrukWideCyr-Bold'
+
+        secondary: {
+          primary: '#FFFFFF',
+          secondary: '#F9F8F8',
+          light: '#EFEFEF',
+          wrapper: {
+            dark: '#000000',
+            light: '#DCDCDC'
+          }
+        },
+
+        tomato: {
+          primary: '#DA0404',
+          secondary: '#C70000'
+        },
+
+        dark: {
+          secondary: {
+            primary: '#000000',
+            secondary: '',
+            light: '',
+            wrapper: {
+              dark: '#FFFFFF',
+              light: ''
+            }
+          }
         }
-    },
-    variants: {},
-    plugins: [],
-    purge: {}
+      },
+
+      fontFamily: {
+        druk: ['DrukWideCyr-Bold']
+      }
+    }
+  },
+
+  plugins: []
 }
